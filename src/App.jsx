7c5f5a29f6e869d4c4a9ab4ea870424629bf1a8e
EdoardoProstamo <edoardoprostamo.19@libero.it>
2025-04-09 19:30:02 +0200

    feat: aggiunta di un nuovo elemento con push

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,31 @@ import liquors from "./data/liquors";
 
 function App() {
 
-  const [alcolType, setAlcolType] = useState(null);
+  const [alcolType, setAlcolType] = useState(liquors[0]);
   const [newAlcolType, setNewAlcolType] = useState('');
 
 
-  const updatePost = e => setAlcolType(e.target.value);
-  const showSubmitt = e => { e.preventDefault() };
+  const updatePost = e => setNewAlcolType(e.target.value);
+
+  const showSubmitt = e => {
+
+    e.preventDefault();
+
+    //  const alcol = [...alcolType]; => spread operator
+    const alcol = [];
+    alcolType.forEach(alcolType => alcol.push(alcolType));
+    setAlcolType(alcol);
+
+
+  };
 
   return (
     <>
+      <ul>
+        {liquors.map(liquore => <li key={liquore.id}>{liquore.title}</li>)}
+      </ul>
+
       <form onSubmit={showSubmitt}>
-        <ul>
-          {liquors.map(liquore => <li key={liquore.id}>{liquore.title}</li>)}
-        </ul>
         <input type="text" placeholder="Inserisci il nuovo alcolico" onChange={updatePost} />
         <button>INVIO</button>
       </form>
