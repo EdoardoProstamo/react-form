b4efb31979009a1293ce0c3c31b2b132a394f5f6
EdoardoProstamo <edoardoprostamo.19@libero.it>
2025-04-09 15:11:15 +0200

    feat: creazione cartella 'data' contenente file array e cartella 'img' contenente le immagini

---
+++ src/data/articles.js
@@ -0,0 +1,34 @@
+const articles = [
+    {
+        id: "1",
+        title: "Vodka",
+        description: "La vodka è un tipo di acquavite ottenuta dalla distillazione di cereali o patate. È originaria di Russia e Polonia e successivamente si è diffusa in Svezia. Al di là del contenuto alcolico, la produzione di vodka può essere distinta in due grandi categorie: vodka pura e vodka aromatizzata.",
+        img: "src\img\vodka.jpg"
+    },
+    {
+        id: "2",
+        title: "Gin",
+        description: "Il gin è una bevanda alcolica, solitamente incolore, ottenuta per distillazione di un fermentato ricavato solitamente da cereali in cui viene messa a macerare una miscela di varie botaniche (possono arrivare fino a 80). Tra queste devono essere sempre presenti i galbuli di ginepro che caratterizzano il profumo e il gusto. Il nome del distillato deriva proprio da questa pianta.",
+        img: "src\img\gin.jpg"
+    },
+    {
+        id: "3",
+        title: "Rum",
+        description: "Il rum è l'acquavite ottenuta dalla distillazione della melassa della canna da zucchero o del suo succo. Vengono identificate, all'interno del vasto panorama della produzione, tipologie di rum che sono riconducibili alle lavorazioni tradizionali utilizzate nelle varie aree dei Caraibi.",
+        img: "src\img\Rum.jpg"
+    },
+    {
+        id: "4",
+        title: "Tequila",
+        description: "La tequila è un distillato originario del Messico, ottenuto dalla fermentazione e successiva distillazione dell'agave blu (Agave tequilana), prodotto originariamente nella località messicana di Tequila e nelle sue vicinanze, nello Stato di Jalisco. Il modo tradizionale di gustare la tequila è quello di servirla in un bicchiere di terracotta accompagnandola con sale e succo di lime posti sul dorso della mano.",
+        img: "src\img\Tequila.jpg"
+    },
+    {
+        id: "5",
+        title: "Triple sec",
+        description: "Il triple sec è un liquore trasparente aromatizzato all'arancia con una gradazione alcolica intorno ai 25°, ma può raggiungere anche i 40-45°. Letteralmente Triple sec in francese significa ‘triplo secco’ che sta per tripla distillazione, ad indicare un prodotto di elevata qualità. 'Sec' in francese è un termine che rinvia infatti direttamente al processo e al prodotto della distillazione alcolica.",
+        img: "src\img\triple-sec.jpg"
+    },
+];
+
+export default articles;
